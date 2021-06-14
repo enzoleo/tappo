7cfe981ffd103d28d018328387a6a220dea3233b
Ai Miyano <enzoliao95@gmail.com>
2021-06-14 22:56:06 +0800

    First version of argbase class

--- tappo/args.hpp
+++ tappo/args.hpp
@@ -1,7 +1,19 @@
 #ifndef TAPPO_ARGS_HPP_
 #define TAPPO_ARGS_HPP_
 
+#include <string>
+
 namespace tappo {
+
+class ArgBase {
+public:
+  // The default constructor.
+  ArgBase() = default;
+
+protected:
+  // The description of this argument.
+  std::string desc_;
+};
 
 } // namespace tappo
 
