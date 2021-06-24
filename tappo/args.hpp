aeca576b3f4644c2eb6130ac570c9de84a4ce484
Ai Miyano <enzoliao95@gmail.com>
2021-06-24 22:48:47 +0800

    Use std::optional for argument desc

--- tappo/args.hpp
+++ tappo/args.hpp
@@ -1,6 +1,7 @@
 #ifndef TAPPO_ARGS_HPP_
 #define TAPPO_ARGS_HPP_
 
+#include <optional>
 #include <string>
 
 namespace tappo {
@@ -10,9 +11,14 @@ public:
   // The default constructor.
   ArgBase() = default;
 
+  // Accessors/mutators.
+  const auto& desc() const { return desc_; }
+
 protected:
-  // The description of this argument.
-  std::string desc_;
+  // The description of this argument. Usually one should clearly speficy how
+  // this argument work in its decription string. However, it is allowed to
+  // be empty if not specified, so we declare it optional.
+  std::optional<std::string> desc_;
 };
 
 } // namespace tappo
